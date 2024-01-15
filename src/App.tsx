44e8593fdd64dc75e614531eca858c394fc613b8
mayyas-nakhli <mayyasnakhli@gmail.com>
2024-01-16 00:31:53 +0400

    Fixing the import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg';
+import viteLogo from '../public/vite.svg';
 import './App.css';
 import { useCountStore } from './store';
 
